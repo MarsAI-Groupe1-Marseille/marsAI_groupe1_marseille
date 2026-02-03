ee7bd857cc6d65efaa5cdbe21482db7836b73a1e
Achraf Jarhou <achraf.jarhou@laplateforme.io>
2026-02-03 11:44:03 +0100

    creation model user

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password_hash: { type: DataTypes.STRING }, 
+  full_name: { type: DataTypes.STRING },
+  role: { 
+    type: DataTypes.ENUM('admin', 'jury', 'moderator'), 
+    defaultValue: 'jury' 
+  },
+  invite_token: { type: DataTypes.STRING },
+  google_id: { type: DataTypes.STRING }
+}, {
+  tableName: 'users',
+  timestamps: true, // Active la gestion automatique des dates
+  
+  // 👇 INDISPENSABLE : Pour dire à Sequelize d'utiliser 'created_at' au lieu de 'createdAt'
+  underscored: true 
+});
+
+module.exports = User;
