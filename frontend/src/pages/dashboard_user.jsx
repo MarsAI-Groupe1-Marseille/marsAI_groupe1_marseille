64c0ee40932e17934e6d66f347ac4ba8f87c3d6a
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2026-02-09 16:26:12 +0100

    fichier en cours de réalisation

--- frontend/src/pages/dashboard_user.jsx
+++ frontend/src/pages/dashboard_user.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import {Users, Eye, Pencil, Trash2, UserPlus} from "lucide-react";
+import { Users, Eye, Pencil, Trash2, UserPlus } from "lucide-react";
 
 const UsersList = [
     {
         id: 1,
         name: "Xavier Dupont",
-        email:"xavier.dupont@example.com",
-        role:"Administrateur"
+        email: "xavier.dupont@example.com",
+        role: "Administrateur"
     },
     {
         id: 2,
         name: "Marie Curie",
-        email:"marie.curie@example.com",
-        role:"Utilisateur"
+        email: "marie.curie@example.com",
+        role: "Utilisateur"
     },
     {
         id: 3,
         name: "Bobbie Smith",
-        email:"bobbie.smith@example.com",
-        role:"Jury"
+        email: "bobbie.smith@example.com",
+        role: "Jury"
     },
 ];
 
@@ -31,3 +31,18 @@ function UserRow({ user }) {
             </div>
 
             <span className="text-sm text-violet-400">{user.role}</span>
+
+            <div className="flex gap-3 justify-end col-span-2">
+                <button className="p-2 rounded-lg bg-neutral-800 hover:bg-violet-500 transition">
+                    <Eye size={18} />
+                </button>
+
+                <button className="p-2 rounded-lg bg-neutral-800 hover:bg-violet-500 transition">
+                    <Pencil size={18} />
+                </button>
+
+                <button className="p-2 rounded-lg bg-neutral-800 hover:bg-red-600 transition">
+                    <Trash2 size={18} />
+                </button>
+            </div>
+        </section>
