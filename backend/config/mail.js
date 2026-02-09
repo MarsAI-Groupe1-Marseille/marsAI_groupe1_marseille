60c78030a9a031b59e88b7ef5c551e60f4fd4074
anziz <tonmail@example.com>
2026-02-09 14:46:36 +0100

    changement de transporteur pour gmail

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -3,12 +3,13 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT == 465,
+    port: parseInt(process.env.EMAIL_PORT, 10),
+    secure: parseInt(process.env.EMAIL_PORT, 10) === 465, // true pour 465, false pour 587
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
+
 });
 
 module.exports = transporter;
