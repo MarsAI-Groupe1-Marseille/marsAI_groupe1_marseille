3b491a26ad57a4e0f0b2de3b056fc2e90f3ac821
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2026-02-10 14:30:57 +0100

    données temporaires modifié

--- frontend/src/pages/dashboard_user.jsx
+++ frontend/src/pages/dashboard_user.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Users, Eye, Pencil, Trash2, UserPlus} from "lucide-react";
 
 const users = [
-  { id: 1, name: "Benjamin Lacroix", email: "benjamin@mail.com", role: "Admin" },
-  { id: 2, name: "Diakité Mossad", email: "diakite@mail.com", role: "Jury" },
-  { id: 3, name: "", email: "sofia@mail.com", role: "Utilisateur" },
+  { id: 1, name: "Benjamin Lacroix", email: "benjamin@gmail.com", role: "Admin" },
+  { id: 2, name: "Diakité Mossad", email: "diakite@gmail.com", role: "Jury" },
+  { id: 3, name: "Belinda santabou", email: "belinda@gmail.com", role: "Utilisateur" },
 ];
