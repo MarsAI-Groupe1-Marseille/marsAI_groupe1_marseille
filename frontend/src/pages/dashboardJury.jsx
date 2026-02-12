03309c706b82e42ef47dad9027699b421e5fa02e
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2026-02-12 11:04:32 +0100

    réalisation de données pour le tableau jury

--- frontend/src/pages/dashboardJury.jsx
+++ frontend/src/pages/dashboardJury.jsx
@@ -3,3 +3,37 @@
 import { useState } from "react";
 import {Eye, X, Start, Filter} from "lucide-react";
 
+export default function DashboardJury(){
+    const [Films,setFilms] = useState([
+    {
+    id: 1,
+    title: "Gourou",
+    director: "Yann Gozlan",
+    category: "Court métrage",
+    status: "À évaluer",
+    rating: null,
+    comment: "",
+    },
+    {
+    id: 2,
+    title: "Le Mage Du Kremlin",
+    director: "Olivier Assayas",
+    category: "Documentaire",
+    status: "Approuvé",
+    rating: 6,
+    comment: "Très belle réalisation.",
+    },
+    {
+    id: 3,
+    title: "L'affaire Bojarski",
+    director: "Jean-Paul Salomé",
+    category: "Long métrage",
+    status: "Rejeté",
+    rating: 4,
+    comment: "Manque de rythme.",
+    },
+]);
+
+
+    return();
+}
