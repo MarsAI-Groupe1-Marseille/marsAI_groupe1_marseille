b644487197e2a203a4ed75ff57524edb41e02813
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2026-02-12 11:19:59 +0100

    ajout des commentaires

--- frontend/src/pages/dashboardJury.jsx
+++ frontend/src/pages/dashboardJury.jsx
@@ -4,7 +4,9 @@ import { useState } from "react";
 import {Eye, X, Start, Filter} from "lucide-react";
 
 export default function DashboardJury(){
+    //  on stocke : tous les films assignés au jury, leurs informations,leur statut, leur note, leur commentaire
     const [Films,setFilms] = useState([
+    // Chaque film est un objet : sinon les vrai données peuvent provenir d'une API.
     {
     id: 1,
     title: "Gourou",
@@ -33,6 +35,42 @@ export default function DashboardJury(){
     comment: "Manque de rythme.",
     },
 ]);
+// Ce state sert à : Stocker le film en cours d’évaluation, Ouvrir la modal
+// Quand il est null → modal fermée
+// Quand il contient un film → modal ouverte
+const [selectedFilm, setSelectedFilm] = useState(null);
+// NB: Une modal est une fenêtre pop-up qui s’affiche au-dessus du contenu principal pour montrer des informations
+// ou demander une action, comme évaluer un film, sans quitter la page.
+
+// Permet de filtrer les films
+const [activeFilter, setActiveFilter] = useState("Tous");
+
+// Permet de filtrer les films par statut : "Tous", "À évaluer", "Approuvé", "Rejeté"
+// Si un statut change → les stats se mettent à jour automatiquement.
+
+const stats = {
+    total: films.length,
+    viewed: films.filter((f) => f.status !== "À évaluer").length,
+    approved: films.filter((f) => f.status === "Approuvé").length,
+    rejected: films.filter((f) => f.status === "Rejeté").length,
+};
+
+// Si filtre = "Tous" → on affiche tout
+// Sinon → on affiche seulement les films correspondant au statut
+const filteredFilms =
+    activeFilter === "Tous"
+    ? films
+    : films.filter((f) => f.status === activeFilter);
+
+// On parcourt tous les films
+// On remplace uniquement celui modifié
+// On ferme la modal
+const handleSaveEvaluation = () => {
+    setFilms((prev) =>
+    prev.map((f) => (f.id === selectedFilm.id ? selectedFilm : f))
+    );
+    setSelectedFilm(null);
+};
 
 
     return();
